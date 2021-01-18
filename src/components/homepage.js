4ed8bec5d6032e90da38c98a860a17aba9006e92
Billy Ott <willrichott@gmail.com>
2021-01-18 15:54:11 -0500

    style homepage

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -10,13 +10,13 @@ class Homepage extends React.Component {
             <div className="homepage" style={{backgroundImage: `url('./assets/homepage-background.jpg')`}}>
                 <div className="homepage__top-area"></div>
                 <div className="homepage__center-area">
-                    <div className="homepage__header-container">
-                        <div className="homepage__header"><span className="homepage__special-char">F</span>ULL-STACK</div>
-                        <div className="homepage__header"><span className="homepage__special-char">W</span>EB DEVELOPE<span className="homepage__special-char">R</span></div>
-                        {/* <div className="homepage__header">FULL-STACK</div>
-                        <div className="homepage__header">WEB DEVELOPER</div> */}
+                    <div className="homepage__headers-container">
+                        <div className="homepage__header-container">
+                            <div className="homepage__header"><span className="homepage__special-char">F</span>ULL-STACK</div>
+                            <div className="homepage__header"><span className="homepage__special-char">W</span>EB DEVELOPE<span className="homepage__special-char">R</span></div>
+                        </div>
+                        <div className="homepage__sub-header">BILLY OTT</div>
                     </div>
-                    <div className="homepage__sub-header">BILLY OTT</div>
                     <div className="homepage__start-container">
                         <div className="homepage__start-icon"></div>
                         <div className="homepage__start-text">PRESS START</div>
