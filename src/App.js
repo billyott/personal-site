a85789567228fc80d4f4663ddcebf826b554cc25
Billy Ott <willrichott@gmail.com>
2021-01-22 10:59:45 -0500

    build blog page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Homepage from './components/homepage';
 import MainMenu from './components/main-menu';
 import Portfolio from './components/portfolio';
 import About from './components/about';
+import Blog from './components/blog';
 import Contact from './components/contact';
 import './app.scss';
 
@@ -20,6 +21,9 @@ function App() {
           </Route>
           <Route path="/about">
             <About />
+          </Route>
+          <Route path="/blog">
+            <Blog />
           </Route>
           <Route path="/contact">
             <Contact />
