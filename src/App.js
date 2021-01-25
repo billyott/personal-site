3a604a3c07e154895dc6c4063cc087136cefb067
Billy Ott <willrichott@gmail.com>
2021-01-25 14:19:31 -0500

    add basename to browserrouter

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import './app.scss';
 function App() {
   return (
     <div className="app">
-      <BrowserRouter>
+      <BrowserRouter basename="/">
         <Switch>
           <Route path="/main-menu">
             <MainMenu />
