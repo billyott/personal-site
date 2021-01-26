01df30f2346e58fc30249049ddd8deeec4dd8405
Billy Ott <willrichott@gmail.com>
2021-01-26 15:44:06 -0500

    update portfolio toggle ability

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -3,71 +3,48 @@ import { NavLink } from 'react-router-dom';
 import ReactPlayer from "react-player";
 
 import './portfolio.scss';
+import projects from  '../data/project-data.json';
 
 class Portfolio extends React.Component {
 
     state = {
-        selected: "none"
+        selectedProject: projects[0]
     }
 
-    updateSeletedTwelveMonths = () => {
-        this.setState({selected: "12months"})
+    updateSelectedProject = (project) => {
+        this.setState({selectedProject: project})
     }
 
-    updateSeletedFlashFlip = () => {
-        this.setState({selected: "flash flip"})
+    generatePreviewIcons = () => {
+        return (projects.map(project => {
+            return (
+                <div key={project.id} className="portfolio__project" style={{backgroundImage: `url('${project.prevImg}')`}}>
+                    <button className="portfolio__project-title" onFocus={() => this.updateSelectedProject(project)}>{project.title}</button>
+                </div>
+            )
+        }))
     }
 
-    updateSeletedCompletionist = () => {
-        this.setState({selected: "completionist"})
+    findIndexOfSelectedProject = () => {
+        const index = projects.findIndex(project => project === this.state.selectedProject)
+        return index
     }
 
-    updateSeletedScrambled = () => {
-        this.setState({selected: "scrambled"})
-    }
-
-    generateDataForSelection = () => {
-        if (this.state.selected === "12months") {
-            return({
-                title: "12MONTHS",
-                prevImg: "./assets/twelvemonths-preview.png",
-                stack: "React.js, Ruby on Rails, React Redux",
-                desc: "A minimalist journaling app that allows users to jot down notes, track habits, and monitor mood and sleep trends.",
-                github: "https://github.com/billyott/twelve-months-client",
-                youtube: "https://www.youtube.com/watch?v=Pwc1hHV852g"
-            })
-        } else if (this.state.selected === "flash flip") {
-            return({
-                title: "Flash Flip",
-                prevImg: "./assets/flashflip-preview.png",
-                stack: "React.js, Ruby on Rails",
-                desc: "A flash card application to help with studying any topic. Includes a 'game' feature to help with testing.",
-                github: "https://github.com/billyott/flash-flip-client",
-                youtube: "https://www.youtube.com/watch?v=KvWOtRugWLg"
-            })
-        } else if (this.state.selected === "completionist") {
-            return({
-                title: "Completionist",
-                prevImg: "./assets/completionist-preview.png",
-                stack: "JavaScript, Ruby on Rails",
-                desc: "A basic task tracker.",
-                github: "https://github.com/bfeldman/completionist-frontend",
-                youtube: "https://www.youtube.com/watch?v=hXr_y20hb7g"
-            })
-        }  else if (this.state.selected === "scrambled") {
-            return({
-                title: "Scrambled",
-                prevImg: "./assets/scrambled-preview.png",
-                stack: "Ruby",
-                desc: "A CLI word scramble game written completely in Ruby.",
-                github: "https://github.com/billyott/scrambled",
-                youtube: "https://www.youtube.com/watch?v=K8k6POBPFfU"
-            })
+    toggleNext = () => {
+        if (this.findIndexOfSelectedProject() + 1 === projects.length) {
+            this.setState({selectedProject: projects[0]})
         } else {
-            return({})
+            this.setState({ selectedProject: projects[this.findIndexOfSelectedProject() + 1]})
         }
     }
-    
+
+    toggleBack = () => {
+        if (this.findIndexOfSelectedProject() - 1 === -1) {
+            this.setState({selectedProject: projects[projects.length-1]})
+        } else {
+            this.setState({ selectedProject: projects[this.findIndexOfSelectedProject() - 1]})
+        }
+    }
 
     render() {
         return (
@@ -77,37 +54,23 @@ class Portfolio extends React.Component {
                     <NavLink to="/main-menu" className="portfolio__back-button">GO BACK</NavLink>
                 </div>
                 <div className="portfolio__projects-container">
-                    <div className="portfolio__project" style={{backgroundImage: `url('./assets/twelvemonths-preview.png')`}}>
-                        <button className="portfolio__project-title" onFocus={this.updateSeletedTwelveMonths}>12MONTHS</button>
-                    </div>
-                    <div className="portfolio__project" style={{backgroundImage: `url('./assets/flashflip-preview.png')`}}>
-                        <button className="portfolio__project-title" onFocus={this.updateSeletedFlashFlip}>FLASH FLIP</button>
-                    </div>
-                    <div className="portfolio__project" style={{backgroundImage: `url('./assets/completionist-preview.png')`}}>
-                        <button className="portfolio__project-title" onFocus={this.updateSeletedCompletionist}>COMPLETIONIST</button>
-                    </div>
-                    <div className="portfolio__project" style={{backgroundImage: `url('./assets/scrambled-preview.png')`}}>
-                        <button className="portfolio__project-title" onFocus={this.updateSeletedScrambled}>SCRAMBLED</button>
-                    </div>
+                    {this.generatePreviewIcons()}
                 </div>
-                {this.state.selected === "none" ? null :
+                {this.selectedProject ? null :
                     <div className="portfolio__prev-container">
+                        <button className="portfolio__mobile-toggle portfolio__mobile-toggle--prev" onClick={this.toggleBack}>prev</button>
                         <div className="portfolio__prev-details">
-                            <div className="portfolio__prev-title">{this.generateDataForSelection()['title']}</div>
-                            <div className="portfolio__prev-desc">⚬ {this.generateDataForSelection()['desc']}</div>
-                            <div className="portfolio__prev-stack">⚬ built in {this.generateDataForSelection()['stack']}</div>
+                            <div className="portfolio__prev-title">{this.state.selectedProject['title']}</div>
+                            <div className="portfolio__prev-desc">⚬ {this.state.selectedProject['desc']}</div>
+                            <div className="portfolio__prev-stack">⚬ built in {this.state.selectedProject['stack']}</div>
                             <div className="portfolio__prev-github-container">
-                                <a className="portfolio__prev-github" href={this.generateDataForSelection()['github']} target="_blank" rel="noreferrer">GitHub Repo</a>
+                                <a className="portfolio__prev-github" href={this.state.selectedProject['github']} target="_blank" rel="noreferrer">GitHub Repo</a>
                             </div>
                         </div>
                         <div className="portfolio__player-container">
-                            <ReactPlayer className='portfolio__react-player' url={this.generateDataForSelection()['youtube']} width="100%" height="100%"/>
+                            <ReactPlayer className='portfolio__react-player' url={this.state.selectedProject['youtube']} width="100%" height="100%"/>
                         </div>
-                        {/* <img src={this.generateDataForSelection()['prevImg']} className="portfolio__prev-prev-img" alt="screenshot of project ui"></img> */}
-                        {/* <div className="portfolio__prev-demo">
-                            <div className="portfolio__prev-demo-title"></div>
-                            <div className="portfolio__prev-demo-vid"></div>
-                        </div> */}
+                        <button className="portfolio__mobile-toggle portfolio__mobile-toggle--next" onClick={this.toggleNext}>next</button>
                     </div>
                 }
             </div>
