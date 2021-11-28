aa51aec64ba730b91cf881f1a7fd2fd1604a5dad
Billy Ott <willrichott@gmail.com>
2021-11-27 22:39:49 -0500

    update about content

--- src/components/about.js
+++ src/components/about.js
@@ -8,10 +8,22 @@ function About() {
         <div className="about">
             <div className="about__header">ABOUT</div>
             <div className="about__content">
-                <p>
-                Hi! I'm a Full-stack Software Developer with a focus in React.js and Ruby on Rails. I'm also a former digital marketer who dabbled in data analysis, data visualization, team-management, and building things from scratch. In my spare time, I'm usually playing video games, cooking, or learning something new.
-                </p>
                 <img className="about__image" src="./assets/wo-pp-bw.png" alt="Bill Ott selfie" />
+                <p>
+                Hello. Thank you for visiting my site. 
+                <br/>
+                <br/>
+                I'm a full stack developer. These days, I'm mostly writing Javascript and working in frameworks like Vue and React, but I also use Python and Ruby.
+                <br/>
+                <br/>
+                In the past, I worked in digital marketing - managing campaigns, analyzing performance data, and building tools to make it easier. I worked with a lot of cool tech, but I wanted to make cool tech.
+                <br/>
+                <br/>
+                I'm usually coding, learning Japanese, cooking, or playing video games.
+                <br/>
+                <br/>
+                NYC is home.
+                </p>
             </div>
         </div>
     )
