bf12851bc603c7f9e10812a23c512eec716cba1f
Billy Ott <willrichott@gmail.com>
2021-12-22 01:33:28 -0500

    add conditionals for submit

--- src/components/contact-form.js
+++ src/components/contact-form.js
@@ -1,20 +1,25 @@
-import React, { useRef }  from 'react';
+import React, { useRef, useState }  from 'react';
 import emailjs from 'emailjs-com';
 
 import './contact-form.scss';
 
 export default function ContactForm() {
 
+  const [hasSubmitted, setHasSubmitted] = useState(false);
+  const [hadError, setHadError] = useState(false);
+
   const form = useRef();
 
   function sendEmail(e) {
     e.preventDefault();
-    
+
     emailjs.sendForm(process.env.REACT_APP_SABISU, process.env.REACT_APP_TENPURETO, e.target, process.env.REACT_APP_YUZA)
     .then((result) => {
-        console.log(result.text);
+      console.log(result.text);
+      setHasSubmitted(true);
     }, (error) => {
-        console.log(error.text);
+      console.log(error.text);
+      setHadError(true);
     });
 
     e.target.reset();
@@ -22,7 +27,16 @@ export default function ContactForm() {
 
   return (
     <form className='contact-form' ref={form} onSubmit={sendEmail}>
-      <div className='contact-form__thank-you-message'>Thank you for your message! I'll reply as soon as possible!</div>
+      {hasSubmitted && (
+        <div className='contact-form__submit-message'>
+          Thank you for your message! I'll reply as soon as possible!
+        </div>
+      )}
+      {hadError && (
+        <div className='contact-form__submit-message'>
+          Oh no! Issue sending message - please refresh and try again :(
+        </div>
+      )}
       <label className='contact-form__label'>Name</label>
       <input className='contact-form__input' type="text" name="name" />
       <label className='contact-form__label'>Email</label>
