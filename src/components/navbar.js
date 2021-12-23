514b123911be58cff17d7df02fce8f5f4b9e54e2
Billy Ott <willrichott@gmail.com>
2021-12-23 10:31:27 -0500

    remove a tags

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -8,10 +8,10 @@ export default function Navbar() {
     return (
         <div className='navbar'>
             <ul className='navbar__list'>
-                <li className='navbar__list-item'><Link to="homepage" smooth={true} duration={500}><a href="/">Home</a></Link></li>
-                <li className='navbar__list-item'><Link to="about" smooth={true} duration={500}><a href="/">Bio</a></Link></li>
-                <li className='navbar__list-item'><Link to="portfolio" smooth={true} duration={500}><a href="/">Portfolio</a></Link></li>
-                <li className='navbar__list-item'><Link to="contact" smooth={true} duration={500}><a href="/">Contact</a></Link></li>
+                <li className='navbar__list-item'><Link className="navbar__link" to="homepage" smooth={true} duration={500}>Home</Link></li>
+                <li className='navbar__list-item'><Link className="navbar__link" to="about" smooth={true} duration={500}>Bio</Link></li>
+                <li className='navbar__list-item'><Link className="navbar__link" to="portfolio" smooth={true} duration={500}>Portfolio</Link></li>
+                <li className='navbar__list-item'><Link className="navbar__link" to="contact" smooth={true} duration={500}>Contact</Link></li>
             </ul>
         </div>
     );
