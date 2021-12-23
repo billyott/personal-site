936c1f4e0e29846251cd83195d5ac08033286b56
Billy Ott <willrichott@gmail.com>
2021-12-23 10:40:46 -0500

    update spacing

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -8,10 +8,46 @@ export default function Navbar() {
     return (
         <div className='navbar'>
             <ul className='navbar__list'>
-                <li className='navbar__list-item'><Link className="navbar__link" to="homepage" smooth={true} duration={500}>Home</Link></li>
-                <li className='navbar__list-item'><Link className="navbar__link" to="about" smooth={true} duration={500}>Bio</Link></li>
-                <li className='navbar__list-item'><Link className="navbar__link" to="portfolio" smooth={true} duration={500}>Portfolio</Link></li>
-                <li className='navbar__list-item'><Link className="navbar__link" to="contact" smooth={true} duration={500}>Contact</Link></li>
+                <li className='navbar__list-item'>
+                    <Link 
+                        className="navbar__link" 
+                        to="homepage" 
+                        smooth={true} 
+                        duration={500}
+                    >
+                        Home
+                    </Link>
+                </li>
+                <li className='navbar__list-item'>
+                    <Link
+                        className="navbar__link"
+                        to="about"
+                        smooth={true}
+                        duration={500}
+                    >
+                        Bio
+                    </Link>
+                </li>
+                <li className='navbar__list-item'>
+                    <Link
+                        className="navbar__link"
+                        to="portfolio"
+                        smooth={true}
+                        duration={500}
+                    >
+                        Portfolio
+                    </Link>
+                </li>
+                <li className='navbar__list-item'>
+                    <Link
+                        className="navbar__link"
+                        to="contact"
+                        smooth={true}
+                        duration={500}
+                    >
+                        Contact
+                    </Link>
+                </li>
             </ul>
         </div>
     );
